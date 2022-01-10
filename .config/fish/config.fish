dc0aa81f4e033d08fc9c4c9a9d8a34972f8401be
Benzoh <benzoh.wada@gmail.com>
2022-01-10 23:28:07 +0900

    on m1 mac

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,13 @@
 set -x NODENV_ROOT $HOME/.anyenv/envs/nodenv
 set -x PATH $HOME/.anyenv/envs/nodenv/bin $HOME/.anyenv/envs/nodenv/versions/12.16.1/bin $PATH
 set -gx PATH $NODENV_ROOT/shims $PATH
-
-set -x ANDROID_HOME $HOME/Library/Android/sdk
-set -x PATH $PATH $ANDROID_HOME/emulator
-set -x PATH $PATH $ANDROID_HOME/tools
-set -x PATH $PATH $ANDROID_HOME/tools/bin
-set -x PATH $PATH $ANDROID_HOME/platform-tools
+#set -gx DOCKER_HOST tcp://127.0.0.1:2375
+set -gx DOCKER_HOST unix:///Users/hippohack/.lima/docker/sock/docker.sock
+set -gx LIMA_INSTANCE docker
 
 alias g='git'
 alias d='docker'
 alias dc='docker-compose'
+
+status --is-interactive; and source (jump shell fish | psub)
+
