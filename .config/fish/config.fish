96e3217a3e91b9e736ac0e5297990ad4ca1c588f
Benzoh <benzoh.wada@gmail.com>
2022-05-11 00:38:56 +0900

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,11 @@
 set -x NODENV_ROOT $HOME/.anyenv/envs/nodenv
 set -x PATH $HOME/.anyenv/envs/nodenv/bin $HOME/.anyenv/envs/nodenv/versions/12.16.1/bin $HOME/.anyenv/envs/nodenv/versions/14.17.1/bin $PATH
 set -gx PATH $NODENV_ROOT/shims $PATH
-#set -gx DOCKER_HOST tcp://127.0.0.1:2375
-set -gx DOCKER_HOST unix:///Users/hippohack/.lima/docker/sock/docker.sock
-set -gx LIMA_INSTANCE docker
+
+# set -gx DOCKER_HOST tcp://127.0.0.1:2375
+# set -gx DOCKER_HOST unix:///Users/hippohack/.lima/docker_x86_64/sock/docker.sock
+set -x DOCKER_HOST unix:///Users/hippohack/.lima/docker/sock/docker.sock
+set -x LIMA_INSTANCE docker
 
 set -gx DOCKER_CONTENT_TRUST 1
 
