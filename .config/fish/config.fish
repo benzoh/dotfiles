fd4914724e24f1d9c18c85a4fa5976ee51672a19
Benzoh <benzoh.wada@gmail.com>
2022-09-30 22:42:11 +0900

    🥦

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 set -x NODENV_ROOT $HOME/.anyenv/envs/nodenv
-set -x PATH $HOME/.anyenv/envs/nodenv/bin $HOME/.anyenv/envs/nodenv/versions/12.16.1/bin $HOME/.anyenv/envs/nodenv/versions/14.17.1/bin $PATH
-set -gx PATH $NODENV_ROOT/shims $PATH
+set -x PATH $HOME/.anyenv/envs/rbenv/bin $HOME/.anyenv/envs/nodenv/bin $HOME/.anyenv/envs/nodenv/versions/12.16.1/bin $HOME/.anyenv/envs/nodenv/versions/14.17.1/bin $PATH
+set -gx PATH $HOME/.rbenv/shims $NODENV_ROOT/shims $PATH
 
 # set -gx DOCKER_HOST tcp://127.0.0.1:2375
 # set -gx DOCKER_HOST unix:///Users/benzoh/.lima/docker_x86_64/sock/docker.sock
@@ -11,11 +11,12 @@ set -x LIMA_INSTANCE docker
 set -gx DOCKER_CONTENT_TRUST 0
 
 # php8.0
-set -gx PATH /opt/homebrew/opt/php@8.0/bin $PATH
-set -gx LDFLAGS "-L/opt/homebrew/opt/php@8.0/lib"
-set -gx CPPFLAGS "-I/opt/homebrew/opt/php@8.0/include"
+# set -gx PATH /opt/homebrew/opt/php@8.0/bin $PATH
+# set -gx LDFLAGS "-L/opt/homebrew/opt/php@8.0/lib"
+# set -gx CPPFLAGS "-I/opt/homebrew/opt/php@8.0/include"
+
 # php8.1
-# set -gx PATH /opt/homebrew/opt/php@8.1/bin $PATH
+set -gx PATH /opt/homebrew/opt/php@8.1/bin $PATH
 # set -gx LDFLAGS "-L/opt/homebrew/opt/php@8.1/lib"
 # set -gx CPPFLAGS "-I/opt/homebrew/opt/php@8.1/include"
 
@@ -31,3 +32,6 @@ status --is-interactive; and source (jump shell fish | psub)
 
 # direnv
 direnv hook fish | source
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
